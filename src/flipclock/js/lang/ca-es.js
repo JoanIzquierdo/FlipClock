2f8e00e1ea6d46dd4c10723409e9cb1d5274bc8d
Joan Izquierdo <joanizquierdo@gmail.com>
2015-03-13 11:31:33 +0100

    Create ca-es.js

---
+++ src/flipclock/js/lang/ca-es.js
@@ -0,0 +1,26 @@
+  /**
+   * FlipClock Catalan Language Pack
+   *
+   * This class will used to translate tokens into the Catalan language.
+   *
+   */
+
+  FlipClock.Lang.Catalan = {
+
+    'years'   : 'Anys',
+    'months'  : 'Mesos',
+    'days'    : 'Dies',
+    'hours'   : 'Hores',
+    'minutes' : 'Minuts',
+    'seconds' : 'Segons'
+
+  };
+
+  /* Create various aliases for convenience */
+
+  FlipClock.Lang['ca']      = FlipClock.Lang.Catalan;
+  FlipClock.Lang['ca-es']   = FlipClock.Lang.Catalan;
+  FlipClock.Lang['catalan']  = FlipClock.Lang.Catalan;
+
+}(jQuery));
+
